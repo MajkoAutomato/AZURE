031faec3446a54bf0d4829586d3197394a4aadac
Maik Christoph Lischka <126296940+MajkoAutomato@users.noreply.github.com>
2023-10-07 18:12:01 +0200

    Removed read-host request for PublicIPAddress

--- AZURE_POWERSHELL/VM_LINUX/VM_LINUX_PROMPT/VM_LINUX_PROMPT.ps1
+++ AZURE_POWERSHELL/VM_LINUX/VM_LINUX_PROMPT/VM_LINUX_PROMPT.ps1
@@ -12,21 +12,19 @@ $image = Read-Host "Enter the image for the VM (e.g., Debian)"
 
 $size = Read-Host "Enter the size for the VM (e.g., Standard_B2s)"
 
-$publicIpAddressName = Read-Host "Enter the public IP address name for the VM"
-
 $sshKeyName = Read-Host "Enter the SSH key name for the VM"
 
 
 New-AzResourceGroup -Name $resourceGroupName1 -Location $location
 
-New-AzVm -ResourceGroupName $resourceGroupName2 -Name $resourceGroupName2 -Location $location -Image $image -Size $size -PublicIpAddressName $publicIpAddressName -OpenPorts 80 -GenerateSshKey -SshKeyName $sshKeyName
+New-AzVm -ResourceGroupName $resourceGroupName2 -Name $resourceGroupName2 -Location $location -Image $image -Size $size -OpenPorts 80 -GenerateSshKey -SshKeyName $sshKeyName
 
 $scriptFile = ".\testdemolinux.sh"
 echo 'sudo apt-get update && sudo apt-get install -y nginx' > $scriptFile
 
 Invoke-AzVMRunCommand -ResourceGroup $resourceGroupName1 -Name $resourceGroupName1 -CommandId RunShellScript -ScriptPath $scriptFile
 
-Get-AzPublicIpAddress -ResourceGroupName $resourceGroupName2 -Name $publicIpAddressName | select "IpAddress"
+Get-AzPublicIpAddress -ResourceGroupName $resourceGroupName2 -Name | select "IpAddress"
 
 $confirmRemove = Read-Host "Do you want to remove the resource group $resourceGroupName1? (yes/no)"
 if ($confirmRemove -eq 'yes') {
